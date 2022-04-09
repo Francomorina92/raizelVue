185c8342738e9fde4c51f564187265a73bb20dd8
Franco Morina <francomorina@outlook.com>
2022-04-09 10:38:24 -0300

    Menu desplegable

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -31,6 +31,220 @@
           :key="link.title"
           v-bind="link"
         />
+          <div class="cursor-pointer non-selectable">
+            <q-item
+              clickable            
+            >
+              <q-item-section class="icono"
+                avatar
+              > 
+                <q-icon name="fa-solid fa-gears" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>Administrar</q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-menu anchor="top end" self="top start">
+              <!-- nivel 1 -->
+              <q-list class="no-border-radius" style="min-width: 80px; max-width: 180px">
+                <!-- Categorias -->
+                <q-item clickable dense>
+                  <q-item-section>
+                    <q-item-label>Categorias</q-item-label>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-item-section class="icono"
+                      avatar
+                    > 
+                      <q-icon name="fa-solid fa-angle-right"/>
+                    </q-item-section>
+                  </q-item-section>
+                  <q-menu anchor="top end" self="top start">
+                    <!-- Nivel 2 -->
+                    <q-list class="no-border-radius">
+                      <!-- Crear -->
+                      <q-item
+                        dense
+                        clickable
+                        to="/category/create"
+                      >
+                        <q-item-section>
+                          <q-item-label>Crear</q-item-label>
+                        </q-item-section>
+                        <q-item-section class="icono"
+                          avatar
+                        > 
+                          <q-icon name="fa-solid fa-circle-plus"/>
+                        </q-item-section>
+                      </q-item>
+                      <!-- Editar -->
+                      <q-item
+                        dense
+                        clickable
+                        to="/category/edit"
+                      >
+                        <q-item-section>
+                          <q-item-label>Editar</q-item-label>
+                        </q-item-section>
+                        <q-item-section class="icono"
+                          avatar
+                        > 
+                          <q-icon name="fa-solid fa-pen-to-square"/>
+                        </q-item-section>
+                      </q-item>
+                      <!-- Lista -->
+                      <q-item
+                        dense
+                        clickable
+                        to="/category/list"
+                      >
+                        <q-item-section>
+                          <q-item-label>Lista</q-item-label>
+                        </q-item-section>
+                        <q-item-section class="icono"
+                          avatar
+                        > 
+                          <q-icon name="fa-solid fa-clipboard-list"/>
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu>
+                </q-item>
+                
+
+                <q-separator />
+                <!-- Equipamientos -->
+                <q-item clickable dense>
+                  <q-item-section>
+                    <q-item-label>Equipamientos</q-item-label>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-item-section class="icono"
+                      avatar
+                    > 
+                      <q-icon name="fa-solid fa-angle-right"/>
+                    </q-item-section>
+                  </q-item-section>
+                  <q-menu anchor="top end" self="top start">
+                    <!-- Nivel 2 -->
+                    <q-list>
+                      <!-- Crear -->
+                      <q-item
+                        dense
+                        clickable
+                        to="/equipament/create"
+                      >
+                        <q-item-section>
+                          <q-item-label>Crear</q-item-label>
+                        </q-item-section>
+                        <q-item-section class="icono"
+                          avatar
+                        > 
+                          <q-icon name="fa-solid fa-circle-plus"/>
+                        </q-item-section>
+                      </q-item>
+                      <!-- Editar -->
+                      <q-item
+                        dense
+                        clickable
+                        to="/equipament/edit"
+                      >
+                        <q-item-section>
+                          <q-item-label>Editar</q-item-label>
+                        </q-item-section>
+                        <q-item-section class="icono"
+                          avatar
+                        > 
+                          <q-icon name="fa-solid fa-pen-to-square"/>
+                        </q-item-section>
+                      </q-item>
+                      <!-- Lista -->
+                      <q-item
+                        dense
+                        clickable
+                        to="/equipament/list"
+                      >
+                        <q-item-section>
+                          <q-item-label>Lista</q-item-label>
+                        </q-item-section>
+                        <q-item-section class="icono"
+                          avatar
+                        > 
+                          <q-icon name="fa-solid fa-clipboard-list"/>
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu>
+                </q-item>
+                <q-separator />
+                <!-- Musculos -->
+                <q-item clickable dense>
+                  <q-item-section>
+                    <q-item-label>Musculos</q-item-label>
+                  </q-item-section>
+                  <q-item-section side>
+                    <q-item-section class="icono"
+                      avatar
+                    > 
+                      <q-icon name="fa-solid fa-angle-right"/>
+                    </q-item-section>
+                  </q-item-section>
+                  <q-menu anchor="top end" self="top start">
+                    <!-- Nivel 2 -->
+                    <q-list>
+                      <!-- Crear -->
+                      <q-item
+                        dense
+                        clickable
+                        to="/muscle/create"
+                      >
+                        <q-item-section>
+                          <q-item-label>Crear</q-item-label>
+                        </q-item-section>
+                        <q-item-section class="icono"
+                          avatar
+                        > 
+                          <q-icon name="fa-solid fa-circle-plus"/>
+                        </q-item-section>
+                      </q-item>
+                      <!-- Editar -->
+                      <q-item
+                        dense
+                        clickable
+                        to="/muscle/edit"
+                      >
+                        <q-item-section>
+                          <q-item-label>Editar</q-item-label>
+                        </q-item-section>
+                        <q-item-section class="icono"
+                          avatar
+                        > 
+                          <q-icon name="fa-solid fa-pen-to-square"/>
+                        </q-item-section>
+                      </q-item>
+                      <!-- Lista -->
+                      <q-item
+                        dense
+                        clickable
+                        to="/muscle/list"
+                      >
+                        <q-item-section>
+                          <q-item-label>Lista</q-item-label>
+                        </q-item-section>
+                        <q-item-section class="icono"
+                          avatar
+                        > 
+                          <q-icon name="fa-solid fa-clipboard-list"/>
+                        </q-item-section>
+                      </q-item>
+                    </q-list>
+                  </q-menu>
+                </q-item>
+                
+              </q-list>
+            </q-menu>
+          </div>
         <div class="fixed-bottom">
         <EssentialLink
           v-for="link in links"
@@ -128,5 +342,8 @@ export default defineComponent({
   }
   .absolute{
     position: absolute;
+  }
+  .icono{
+    color: #2CB1BC;
   }
 </style>
