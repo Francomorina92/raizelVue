bf59cf61376991755ff12c653404ba96d17a3c01
Franco Morina <francomorina@outlook.com>
2022-04-16 17:12:46 -0300

    [R110871-36] poder registrar equipamientos

--- src/store/equipamientos/actions.js
+++ src/store/equipamientos/actions.js
@@ -2,28 +2,64 @@ import {api} from '../../boot/axios'
 import { date } from 'quasar'
 
 export default {
-    async loadEquipamientos({ commit }) {
-        const {data} = await api.get('/equipamientos')
-        const {rows}=data.equipamientos;
-        if ( !rows ){
-            commit('setEquipamientos', [] )
-            return
+    async loadEquipamientos({ commit }, objeto) {
+        try {
+            commit('setLoading', true )
+            const {data} = await api.get(`/equipamientos/${objeto.limite}/${objeto.desde}`)
+            const {rows} = data.equipamientos;
+            if ( !rows ){
+                commit('setLoading', false )
+                commit('setRespuesta', false )
+                return
+            }else if (rows.length < 10) {
+                commit('setRespuesta', false )
+            }else{
+                commit('setRespuesta', true )
+            }
+            commit('setFrom', rows.length)
+            const rowsFormateada = rows.map(function(x) {
+                x.createdAt = date.formatDate(x.createdAt,'DD-MM-YYYY')
+                x.updatedAt = date.formatDate(x.updatedAt,'DD-MM-YYYY')
+                return x;
+            });
+        
+            const equipamientos = []
+            for( let id of Object.keys( rowsFormateada ) ) {
+                equipamientos.push({
+                    id,
+                    ...rowsFormateada[id]
+                })
+            }
+        
+            commit('setEquipamientos', equipamientos )
+        } catch (error) {
+            commit('setError', error )
+        } finally{
+            commit('setLoading', false )
         }
-        const rowsFormateada = rows.map(function(x) {
-            x.createdAt = date.formatDate(x.createdAt,'DD-MM-YYYY')
-            x.updatedAt = date.formatDate(x.updatedAt,'DD-MM-YYYY')
-            return x;
-         });
-    
-        const equipamientos = []
-        for( let id of Object.keys( rowsFormateada ) ) {
-            equipamientos.push({
-                id,
-                ...rowsFormateada[id]
-            })
+    },
+    async setEquipamiento({ commit },objeto) {
+        try {
+            commit('setLoading', true )
+            const {data} = await api.post('/equipamientos',objeto)
+            if ( !data ){
+                return
+            }
+            const dataFormateada = {
+                createdAt: date.formatDate(data.createdAt,'DD-MM-YYYY'),
+                updatedAt: date.formatDate(data.updatedAt,'DD-MM-YYYY'),
+                estado: data.estado ? 'Activo' : 'Desactivado',
+                id: data.id,
+                nombre: data.nombre
+            };
+            commit('setEquipamiento', dataFormateada )
+            return data;
+        } catch (ex) {
+            commit('setError', ex.response.data.errors )
+        }finally{
+            commit('setLoading', false )
         }
-    
-        commit('setEquipamientos', equipamientos )
+        
     }
 }
 
--- src/store/equipamientos/mutations.js
+++ src/store/equipamientos/mutations.js
@@ -1,6 +1,27 @@
 export default{
     setEquipamientos : ( state, equipamientos ) => {
-        state.equipamientos = [ ...state.equipamientos, ...equipamientos ]
+        const rowsFormateada = equipamientos.map(function(x) {
+            if (state.equipamientos.filter(c => c.id !== x.id? false : true)){
+                return x;
+            }
+        });
+        state.equipamientos = [ ...state.equipamientos, ...rowsFormateada ]
         state.isLoading = false
+    },
+    setEquipamiento : ( state, equipamiento ) => {
+        state.equipamientos = [ ...state.equipamientos, equipamiento ]
+        state.isLoading = false
+    },
+    setRespuesta : ( state, respuesta ) => {
+        state.respuesta = respuesta
+    },
+    setLoading : ( state, estado ) => {
+        state.isLoading = estado
+    },
+    setError : ( state, error ) => {
+        state.error = error
+    },
+    setFrom : ( state, from ) => {
+        state.from += from
     }
 }
