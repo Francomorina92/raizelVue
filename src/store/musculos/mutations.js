a275a4cb29c73fbd732d9d6cc95315e44875ffdb
Franco Morina <francomorina@outlook.com>
2022-04-16 17:31:44 -0300

    [R110871-46] poder registrar musculos

--- src/store/musculos/mutations.js
+++ src/store/musculos/mutations.js
@@ -1,6 +1,27 @@
 export default{
     setMusculos : ( state, musculos ) => {
-        state.musculos = [ ...state.musculos, ...musculos ]
+        const rowsFormateada = musculos.map(function(x) {
+            if (state.musculos.filter(c => c.id !== x.id? false : true)){
+                return x;
+            }
+        });
+        state.musculos = [ ...state.musculos, ...rowsFormateada ]
         state.isLoading = false
+    },
+    setMusculo : ( state, musculo ) => {
+        state.musculos = [ ...state.musculos, musculo ]
+        state.isLoading = false
+    },
+    setRespuesta : ( state, respuesta ) => {
+        state.respuesta = respuesta
+    },
+    setLoading : ( state, estado ) => {
+        state.isLoading = estado
+    },
+    setError : ( state, error ) => {
+        state.error = error
+    },
+    setFrom : ( state, from ) => {
+        state.from += from
     }
 }
